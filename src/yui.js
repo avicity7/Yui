68635463e9ba09ee83176a2f4c29bd176fdc8f0d
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-01-09 19:13:34 +0800

    feat: Basic time

--- src/yui.js
+++ src/yui.js
@@ -11,42 +11,74 @@ const parseCommand = (message) => {
     return output
 }
 
+const findTimeframe = (message) => { 
+    let modifier = "";
+    let time = 0;
+    for(let i = 0; i<message.length; i++) {
+        if (isNaN(message[i]) || message[i] == " ") {
+            continue
+        }
+        else{ 
+            let number = message[i]; 
+            for (let x = i+1; x<message.length;x++) {
+                if (message[x] != " ") {
+                    modifier += message[x]
+                }
+            }
+            switch (modifier){
+                case "hours": 
+                    time = 1000 * 60 * 60 * number - 1 
+                case "minutes": 
+                    time = 1000 * 60 * number - 15 
+                case "seconds": 
+                    time = 1000 * number
+            }
+
+        }
+
+    }
+    return time 
+}
+
+const sendReminder = (message) => { 
+    message.reply("Yui here!\n\nYour presentation is close!")
+}
 const yuiMain = (message) => {
     let command = parseCommand(message.body.toLowerCase()).trim();
-    switch(command) { 
-        case 'introduce yourself':
-            message.reply("Yui here!\n\nI'm Karl's AI Assistant. Ask me a question or give me tasks! I listen to all messages starting with 'hey yui'.");
-            break;
+    console.log(command);
+    if (command.includes("presentation") || command.includes("assignment due")) { 
+        if (command.includes("presentation")) {
+            message.reply("Yui here!\n\nI'll alert everyone when your presentation is close.");
+            setTimeout(sendReminder,findTimeframe(command),message);
+        }
+        else {
+            message.reply("Yui here!\n\nI'll alert everyone when your presentation due date is close.")
+        }
+    }
+    else{
+        switch(command) { 
+            case 'introduce yourself':
+                message.reply("Yui here!\n\nI'm Karl's AI Assistant. Ask me a question or give me tasks! I listen to all messages starting with 'hey yui'.");
+                break;
 
-        case 'how much do i owe karl?':
-        case 'how much do i owe': 
-            message.reply('Yui here! You currently owe Karl '+100);
-            break;
-
-        case "fk u":
-        case "fuck you":
-        case "fuck off":
-            message.reply('Yui here! Fuck you!');
-            break;
- 
-
-        default:
-            if(command.startsWith('re!') == false){
-                const response = openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: command,
-                    temperature: 0.3,
-                    max_tokens: 300,
-                    top_p: 1.0,
-                    frequency_penalty: 0.0,
-                    presence_penalty: 0.0,
-                });
-                
-                response.then((result) => {
-                    message.reply("Yui here! "+ result.data.choices[0].text);
-                })
-            }
-            break;
+            default:
+                if(command.startsWith('re!') == false){
+                    const response = openai.createCompletion({
+                        model: "text-davinci-003",
+                        prompt: command,
+                        temperature: 0.3,
+                        max_tokens: 300,
+                        top_p: 1.0,
+                        frequency_penalty: 0.0,
+                        presence_penalty: 0.0,
+                    });
+                    
+                    response.then((result) => {
+                        message.reply("Yui here! "+ result.data.choices[0].text);
+                    })
+                }
+                break;
+        }
     }
 }
 
