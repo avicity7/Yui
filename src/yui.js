c32b6e840eb22b2618ca1b9b8358a4584200b1a0
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-01-10 08:55:39 +0800

    feat: Turned down temperature

--- src/yui.js
+++ src/yui.js
@@ -66,7 +66,7 @@ const yuiMain = (message) => {
                     const response = openai.createCompletion({
                         model: "text-curie-001",
                         prompt: "Respond to this in a cute, girly way: "+ command,
-                        temperature: 0.9,
+                        temperature: 0.75,
                         max_tokens: 300,
                         top_p: 1.0,
                         frequency_penalty: 0.0,
