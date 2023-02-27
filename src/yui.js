4c04fb8c5047608289b3e3582ad76c4d83f4cdef
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-02-27 14:33:07 +0800

    feat: eval

--- src/yui.js
+++ src/yui.js
@@ -1,6 +1,5 @@
 const qrcode = require('qrcode-terminal');
 const openai = require('./openA1');
-const {addPreviousID, readPreviousIDList} = require('./readWrite');
 
 const { Client,LocalAuth } = require('whatsapp-web.js');
 
@@ -12,51 +11,33 @@ const parseCommand = (message) => {
     return output
 }
 
-const sendReminder = (message,reply) => { 
-    message.reply("Yui here!\n\n"+reply)
-}
-
 const yuiMain = (message) => {
     let command = parseCommand(message.body.toLowerCase()).trim();
     console.log(command);
-    if (command.includes("presentation") || command.includes("assignment due")) { 
-        if (command.includes("presentation")) {
-            const {time,reply} = findTimeframe(command);
-            message.reply("Yui here!\n\n"+reply);
-            if (time != null && reply != null) {
-                setTimeout(sendReminder,time,message,"Your presentation ");
-            }
-        }
-        else {
-            message.reply("Yui here!\n\nI'll alert everyone when your presentation due date is close.")
-        }
-    }
-    else{
-        switch(command) { 
-            case 'introduce yourself':
-                message.reply("Yui here!\n\nI'm Karl's AI Assistant. Ask me a question or give me tasks! I listen to all messages starting with 'hey yui'.");
-                break;
+    switch(command) { 
+        case 'introduce yourself':
+            message.reply("Yui here!\n\nI'm Karl's AI Assistant. Ask me a question or give me tasks! I listen to all messages starting with 'hey yui'.");
+            break;
 
-            default:
-                if(command.startsWith('re!') == false){
-                    const response = openai.createCompletion({
-                        model: "text-davinci-003",
-                        prompt: "Using JavaScript and message.reply(), respond to the prompt or answer the question provided in plaintext and no explanation: "+command,
-                        previous_id: "cmpl-6YOthcqqJTZC1yJ05yMETgZq5JP09",
-                        temperature: 0.3,
-                        max_tokens: 300,
-                        top_p: 1.0,
-                        frequency_penalty: 0.0,
-                        presence_penalty: 0.0,
-                    });
-                    
-                    response.then((result) => {
-                        eval(result.data.choices[0].text);
-                    })
-                }
-                break;
+        default:
+            if(command.startsWith('re!') == false){
+                const response = openai.createCompletion({
+                    model: "text-davinci-003",
+                    prompt: "Using JavaScript and message.reply(), respond to the prompt or answer the question provided in plaintext and no explanation: "+command,
+                    previous_id: "cmpl-6YOthcqqJTZC1yJ05yMETgZq5JP09",
+                    temperature: 0.3,
+                    max_tokens: 300,
+                    top_p: 1.0,
+                    frequency_penalty: 0.0,
+                    presence_penalty: 0.0,
+                });
+                
+                response.then((result) => {
+                    eval(result.data.choices[0].text);
+                })
+            }
+            break;
         }
-    }
 }
 
 const client = new Client({
